bba9e5a3d5aef8336231d1f15051188386cc21ad
ToTenPatryk <patrykdyczek@gmail.com>
2026-04-19 18:20:34 +0200

    Update schemat.sql

--- database/schemat.sql
+++ database/schemat.sql
@@ -11,7 +11,8 @@ CREATE TABLE Uzytkownik (
 CREATE TABLE Incydent (
     id_incydentu SERIAL PRIMARY KEY,
     typ VARCHAR(50) NOT NULL,
-    lokalizacja POINT NOT NULL, --koordynaty bo tak to ustalilismy
+    lat DECIMAL(9,6) NOT NULL, -- Szerokość geograficzna
+    lng DECIMAL(9,6) NOT NULL, -- Długość geograficzna
     priorytet VARCHAR(10) NOT NULL CHECK (priorytet IN ('niski', 'średni', 'wysoki', 'krytyczny')),
     status VARCHAR(20) NOT NULL,
     id_uzytkownika INTEGER REFERENCES Uzytkownik(id_uzytkownika)
@@ -25,7 +26,7 @@ CREATE TABLE Operacja (
     id_incydentu INTEGER REFERENCES Incydent(id_incydentu) NOT NULL 
 );
 
--- 4. Tabela Zasoby
+
 CREATE TABLE Zasob (
     id_zasobu SERIAL PRIMARY KEY,
     nazwa VARCHAR(100) NOT NULL,
@@ -35,7 +36,7 @@ CREATE TABLE Zasob (
     dostepnosc BOOLEAN NOT NULL DEFAULT TRUE 
 );
 
--- 5. Tabela łącząca Operacje z Zasobami (M:N)
+
 CREATE TABLE Jednostka (
     id_operacji INTEGER REFERENCES Operacja(id_operacji),
     id_zasobu INTEGER REFERENCES Zasob(id_zasobu),
