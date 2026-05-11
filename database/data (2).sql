54ebb2140df67b6983db0423f70264f70388d78a
ToTenPatryk <patrykdyczek@gmail.com>
2026-05-11 21:22:08 +0200

    Add files via upload

---
+++ database/data (2).sql
@@ -0,0 +1,29 @@
+
+INSERT INTO uzytkownik (imie, nazwisko, rola, login, haslo) VALUES 
+('Mike', 'Administrator', 'admin', 'admin', 'admin123'),           
+('John', 'Dispatcher', 'dyspozytor', 'dispatcher', 'dispatch123'),  
+('Sarah', 'Rescuer', 'ratownik', 'rescuer', 'rescue123');           
+
+
+INSERT INTO zasob (nazwa, typ, specjalizacja, status, dostepnosc,lat,lng) VALUES 
+('Dr. Emily Chen', 'Medical', 'Emergency Medicine', 'Available', TRUE,51.02,17.02), 
+('Fire Unit Alpha', 'Unit', 'Fire Response', 'Available', TRUE, 52.07,18.2),        
+('Ambulance A-12', 'Equipment', 'Life Support', 'Available', TRUE, 50.02, 18.1),    
+('Fire Truck F-5', 'Equipment', 'Water Pump', 'Available', TRUE, 50.5, 17.7);       
+
+
+INSERT INTO incydent (typ, lat, lng, priorytet, status, id_uzytkownika) VALUES 
+('Medical', 51.107883, 17.038538, 'krytyczny', 'w toku', 2), 
+('Fire', 51.112000, 17.055000, 'wysoki', 'zgłoszony', 2);    
+
+
+INSERT INTO operacja (data_rozpoczecia, status, id_incydentu) VALUES 
+(CURRENT_TIMESTAMP, 'aktywna', 1), 
+(CURRENT_TIMESTAMP, 'planowana', 2); 
+
+
+INSERT INTO jednostka (id_operacji, id_zasobu) VALUES 
+(1, 1),
+(1, 3),
+(2, 2), 
+(2, 4); 
